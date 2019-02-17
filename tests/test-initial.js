96accd259ddb849b4764fe073fbab8b284e994f4
Shane Scott <shshan3@gmail.com>
2019-02-16 21:35:49 -0500

    added simple test and setting up CI

---
+++ tests/test-initial.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, closeServer, runServer} = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+
+describe('Root', function() {
+
+
+
+before(function() {
+    return runServer();
+});
+
+after(function() {
+    return closeServer();
+});
+
+    it('should serve static assets on connect', function(done) {
+        return chai
+        .request(app)
+        .get('/')
+        .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+        });
+    });
+});
